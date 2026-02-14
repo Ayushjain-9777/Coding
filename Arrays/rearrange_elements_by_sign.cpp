bb5a87e43736abe533f5fb5fc23775c7e5abe3e7
Ayush Jain <ayushjain9777@gmail.com>
2026-02-14 15:44:34 +0530

    modified reaarange elements

--- Arrays/rearrange_elements_by_sign.cpp
+++ Arrays/rearrange_elements_by_sign.cpp
@@ -37,8 +37,47 @@ int main()
     return 0;
 }
 
+//Brute force Approach
 vector<int> rearrangeArray(vector<int>& nums)
 {
-    vector<int> p1(nums.size()/2), p2(nums.size()/2);
-    
+    vector<int> pos, neg;
+
+    for(int i=0; i<nums.size(); ++i)
+    {
+        if(nums[i]>0) pos.push_back(nums[i]);
+        else neg.push_back(nums[i]);
+    }
+    int idx1 = 0, idx2 = 0;
+    for(int i=0; i<nums.size(); ++i)
+    {
+        if(i%2==0)
+            nums[i] = pos[idx1++];
+        else 
+            nums[i] = neg[idx2++];
+    }
+
+    return nums;
+}
+
+//Optimal Approach
+vector<int> rearrangeArray(vector<int>& nums)
+{
+    vector<int> ans(nums.size(),0);
+    int posIdx = 0, negIdx = 1;
+
+    for(int i=0; i<nums.size(); ++i)
+    {
+        if(nums[i]>0)
+        {
+            ans[posIdx] = nums[i];
+            posIdx += 2;
+        }
+        else 
+        {
+            ans[negIdx] = nums[i];
+            negIdx += 2;
+        }
+    }
+
+    return ans;
 }
