b3c64c77610c89aabd7827d126eb46db66fd7eef
Ayush Jain <ayushjain9777@gmail.com>
2026-02-18 14:49:47 +0530

    pascal's triangle

---
+++ Arrays/pascal's_triangle.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> generate(int numRows);
+void show(vector<vector<int>> pascal);
+
+int main()
+{
+    int n;
+    vector<vector<int>> pascal;
+
+    cin>>n;
+    pascal = generate(n);
+    show(pascal);
+
+    return 0;
+}
+
+vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> pascal;
+    for(int i=0; i<numRows; ++i) {
+        vector<int> row(i+1,1);
+        for(int j=1; j<i; ++j) {
+            row[j] = pascal[i-1][j-1] + pascal[i-1][j]; 
+        }
+        pascal.push_back(row);
+    }
+
+    return pascal;
+}
+
+void show(vector<vector<int>> pascal)
+{
+    for(int i=0; i<pascal.size(); ++i) {
+        for(int j=0; j<pascal[i].size(); ++j) {
+            cout<<pascal[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
