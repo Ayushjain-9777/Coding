e312114721b0a1a7ccf97e017ad15ba489366c9c
Ayush Jain <ayushjain9777@gmail.com>
2026-02-26 14:05:42 +0530

    count subarrays with xor k

---
+++ Arrays/count_subarrays_with_xor_k.cpp
@@ -0,0 +1,52 @@
+//check optimal approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int countSubarrays(vector<int> nums, int k);
+
+int main()
+{
+    vector<int> nums; 
+    int k;
+    int result;
+
+    nums = {4,2,2,6,4};
+    k = 6;
+    result = countSubarrays(nums,k);
+    for(int i=0; i<nums.size(); ++i) {
+        cout<<nums[i]<<"\t";
+    }
+    cout<<endl;
+    cout<<"No. of subarrays: "<<result<<endl;
+    cout<<endl;
+
+    nums = {5,6,7,8,9};
+    k = 5;
+    result = countSubarrays(nums,k);
+    for(int i=0; i<nums.size(); ++i) {
+        cout<<nums[i]<<"\t";
+    }
+    cout<<endl;
+    cout<<"No. of subarrays: "<<result<<endl;
+    cout<<endl;
+
+    return 0;
+}
+
+//Brute force Approach
+int countSubarrays(vector<int> nums, int k) {
+    int count = 0;
+    int val = 0;
+
+
+    for(int i=0; i<nums.size(); ++i) {
+        val = 0;
+        for(int j=i; j<nums.size(); ++j) {
+            val ^= nums[j];
+            if(val==k) count++;
+        }
+    }
+
+    return count;
+}
+
